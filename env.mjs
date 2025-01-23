52fb4b46a66ba211baca578f5da4ba5830c32645
Difint <yarm-public@protonmail.com>
2025-01-23 22:41:19 +0700

    - Updated libs

--- env.mjs
+++ env.mjs
@@ -3,9 +3,10 @@ import { z } from "zod"
 
 export const env = createEnv({
   server: {
+    // Add server-side environment variables here
   },
   client: {
-    NEXT_PUBLIC_APP_URL: z.string().min(1),
+    NEXT_PUBLIC_APP_URL: z.string().url(),
   },
   runtimeEnv: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
